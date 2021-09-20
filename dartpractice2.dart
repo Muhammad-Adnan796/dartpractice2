0f4bfc5c6fe5d5f98c22528545402c1871cd5c01
Muhammad Adnan <79781004+Adnan-Swati@users.noreply.github.com>
2021-09-20 10:04:23 -0700

    Add files via upload

---
+++ dartpractice2.dart
@@ -0,0 +1,72 @@
+void main() {
+ int a = 18;
+  print(a);
+    int b =122; 
+  print (b+a+2);
+  int c = a+b;
+  print (c);
+  double a = 12;
+  print (a);
+  double b = 12.2;
+  print (b);
+  double c = a+b;
+  print (c);
+  print (a+b+c);
+  num a =12;
+  print (a);
+  num b = 12.2;
+  print (b);
+  num c = a+b;
+  print (c);
+  print (a+b+c);
+  String a ='Adnan';
+print (a); 
+  String b = '19';
+  print (b);
+ String  c = '12.2';
+  print (c);
+  print (a+b+c);
+  print ('a+b+c');
+  print ('a${b+c} *${2} = ${4*5}');
+  String d = 'nandaswati8123796@gmail.com';
+  print (d);
+     int a1 = 20;
+  int a2 = 20;
+  bool result = a1<a2;
+  print (result);
+  
+  int a1 = 22;
+  int a2 = 25;
+  bool result = a1<a2;
+  print (result);
+  
+  double  a1 = 20;
+  double a2 = 24;
+  bool result = a1<a2;
+  print (result);
+
+   num  a1 = 204.5;
+  num a2 = 24;
+  bool result = a1<a2;
+  print (result);
+  
+  double a1 = 12.2;
+  double a2 = 12;
+  print (a1*a2);
+  
+  double a1 = 12.2;
+  double a2 = 12;
+  print (a1/a2);
+  
+   double a1 = 12.2;
+  double a2 = 12;
+  int a3 = a1~/a2;
+  print (a3);
+  
+  
+   double a1 = 12.2;
+  double a2 = 12;
+  int a3 = a1~/a2;
+  print (a3);
+  
+ }
